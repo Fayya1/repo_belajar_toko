cebd0da26c1125b3cbf6ba90b7e6042a3ab5673f
Fayya1 <69461501+Fayya1@users.noreply.github.com>
2020-10-23 08:22:35 +0700

    Migrasi Database

---
+++ 2020_08_25_100125_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('product', function (Blueprint $table) {
+          $table->bigIncrements('id_product');
+          $table->string('nama_product');
+          $table->unsignedBigInteger('id_order');
+
+          $table->foreign('id_order')->references('id_order')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
